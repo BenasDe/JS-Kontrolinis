8a9029498a76f85262b09d00a9126253d1caac51
Benas <benas.denisovas@gmail.com>
2022-09-06 23:51:29 +0300

    god dammit

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,28 +9,32 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
-const ul = document.getElementById('output');
+const ENDPOINT = "cars.json";
+const ul = document.getElementById("output");
 const list = document.createDocumentFragment();
-    fetch(ENDPOINT)
-        .then((cars) => {
-            return cars.json();
-        })
-        .then((data) => {
-            let output = data;
-            output.map(function (output) {
-                let li = document.createElement('li');
-                let login = document.createElement('h4');
-                let avatar_url = document.createElement('span');
-                login.innerHTML = `${output.login}`;
-                avatar_url.innerHTML = `${output.avatar_url}`;
-                li.appendChild(login);
-                li.appendChild(avatar_url);
-                list.appendChild(li);
-            });
-        })
-        .catch(function(error) {
-            console.log(error);
-          });
-      
-        ul.appendChild(list);
+fetch("cars.json")
+  .then((response) => {
+    return response.json();
+  })
+  // fetch(ENDPOINT)
+  //   .then((cars) => {
+  //     return cars.json();
+  //   })
+  .then((data) => {
+    let output = data;
+    output.map(function (output) {
+      let li = document.createElement("li");
+      let brand = document.createElement("h4");
+      let models = document.createElement("span");
+      brand.innerHTML = `${output.brand}`;
+      models.innerHTML = `${output.models}`;
+      li.appendChild(brand);
+      li.appendChild(models);
+      list.appendChild(li);
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+ul.appendChild(list);
